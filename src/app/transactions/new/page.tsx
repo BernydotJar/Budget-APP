c537598965aac624478f07083c7fd6b71ec7184e
BernydotJar <fearlesed@gmail.com>
2026-07-03 11:49:44 -0600

    feat(transactions): refine new transaction page wrapper

--- src/app/transactions/new/page.tsx
+++ src/app/transactions/new/page.tsx
@@ -4,7 +4,9 @@ import { TransactionForm } from '@/components/transactions/transaction-form';
 export default function NewTransactionPage() {
   return (
     <AuthGuard>
-      <TransactionForm />
+      <div className="mx-auto flex w-full max-w-5xl flex-col gap-6">
+        <TransactionForm />
+      </div>
     </AuthGuard>
   );
 }
